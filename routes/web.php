124a34ef20b23b38197e714b368211cba641d9a3
jasonetorres <jasontorres585@icloud.com>
2025-12-12 10:55:21 -0500

    rss feed

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-// --- 1. HOMEPAGE ROUTE (Fetches Hashnode Posts & YouTube Videos) ---
+
 Route::get('/', function () {
 
-    // A. FETCH HASHNODE POSTS
+    //  FETCH HASHNODE POSTS
     $hashnodeQuery = <<<'GQL'
     query GetPosts($host: String!) {
         publication(host: $host) {
@@ -33,14 +33,13 @@ Route::get('/', function () {
 
     $posts = $hashnodeResponse->json('data.publication.posts.edges', []);
 
-    // B. FETCH YOUTUBE VIDEOS
+    //  FETCH YOUTUBE VIDEOS
     $videos = [];
     $apiKey = env('YOUTUBE_API_KEY');
     $channelId = env('YOUTUBE_CHANNEL_ID');
 
     if ($apiKey && $channelId) {
-        // Convert Channel ID (UC...) to Uploads Playlist ID (UU...)
-        // We strip the first 2 characters ('UC') and replace them with 'UU'
+
         $uploadsPlaylistId = 'UU' . substr($channelId, 2);
 
         $youtubeResponse = Http::get('https://www.googleapis.com/youtube/v3/playlistItems', [
@@ -53,14 +52,14 @@ Route::get('/', function () {
         $videos = $youtubeResponse->json('items', []);
     }
 
-    // C. RETURN VIEW WITH BOTH DATA SETS
+
     return view('welcome', [
         'posts' => $posts,
         'videos' => $videos
     ]);
 });
 
-// --- 2. STATIC ROUTES ---
+// ---  STATIC ROUTES ---
 Route::get('/tutorial', function () {
     return view('tutorial');
 });
@@ -69,7 +68,7 @@ Route::get('/aiblog', function () {
     return view('aiblog');
 });
 
-// --- 3. DYNAMIC SINGLE POST ROUTE ---
+// --- DYNAMIC SINGLE POST ROUTE ---
 Route::get('/blog/{slug}', function ($slug) {
     $query = <<<'GQL'
     query GetPost($slug: String!, $host: String!) {
@@ -104,4 +103,67 @@ Route::get('/blog/{slug}', function ($slug) {
     }
 
     return view('post', ['post' => $post]);
+});
+
+// --- 4. RSS FEED ROUTE ---
+Route::get('/rss', function () {
+    // 1. Fetch Posts (Reusing your existing pattern)
+    $query = <<<'GQL'
+    query GetPosts($host: String!) {
+        publication(host: $host) {
+            title
+            posts(first: 20) {
+                edges {
+                    node {
+                        title
+                        brief
+                        slug
+                        publishedAt
+                    }
+                }
+            }
+        }
+    }
+    GQL;
+
+    $response = Http::post('https://gql.hashnode.com', [
+        'query' => $query,
+        'variables' => [
+            'host' => 'film2code.vercel.app'
+        ]
+    ]);
+
+    $data = $response->json('data.publication');
+    $posts = $data['posts']['edges'] ?? [];
+    $siteTitle = $data['title'] ?? 'Jason Torres Portfolio';
+
+    // 2. Build XML Structure
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">';
+    $xml .= '<channel>';
+    $xml .= '<title>' . htmlspecialchars($siteTitle) . '</title>';
+    $xml .= '<link>' . url('/') . '</link>';
+    $xml .= '<description>Latest updates and writings from Jason Torres.</description>';
+    $xml .= '<language>en-us</language>';
+    $xml .= '<atom:link href="' . url('/rss') . '" rel="self" type="application/rss+xml" />';
+
+    foreach ($posts as $edge) {
+        $post = $edge['node'];
+        $postUrl = url('/blog/' . $post['slug']);
+        $pubDate = date(DATE_RSS, strtotime($post['publishedAt']));
+
+        $xml .= '<item>';
+        $xml .= '<title>' . htmlspecialchars($post['title']) . '</title>';
+        $xml .= '<link>' . $postUrl . '</link>';
+        $xml .= '<guid>' . $postUrl . '</guid>';
+        $xml .= '<pubDate>' . $pubDate . '</pubDate>';
+        $xml .= '<description>' . htmlspecialchars($post['brief']) . '</description>';
+        $xml .= '</item>';
+    }
+
+    $xml .= '</channel>';
+    $xml .= '</rss>';
+
+    // 3. Return XML Response
+    return response($xml, 200, ['Content-Type' => 'application/xml']);
 });
